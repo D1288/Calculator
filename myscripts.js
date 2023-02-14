f1af0dde8a730c3c43187222f45be0b155e474e5
Daniel Metz <107957273+D1288@users.noreply.github.com>
2023-02-13 17:12:21 -0800

    added styling for calculator. currently can perform basic math functions

--- myscripts.js
+++ myscripts.js
@@ -10,25 +10,38 @@ const clearButton = document.querySelector(".buttonClear")
 let valueShown = document.querySelector(".valueShown")
 let valueCalc = [];
 
-clearButton.addEventListener('click', ()=>{
+clearButton.addEventListener('click', () => {
     valueCalc = [];
+    valueShown.textContent = valueCalc;
     console.log(valueCalc)
 })
 
-numButtons.forEach((button) =>{
-    button.addEventListener('click', ()=>{
-        
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
         valueCalc.push(button.textContent);
+        valueShown.textContent = valueCalc;
         console.log(valueCalc)
     })
 })
 
-addButton.addEventListener('click', ()=>{
-   valueCalc = [add(valueCalc[0], valueCalc[1])]
-
+addButton.addEventListener('click', () => {
+    console.log(valueCalc)
+    valueCalc = [add(valueCalc[0], valueCalc[1])]
 })
 
-equalButton.addEventListener('click', ()=>{
+subtractButton.addEventListener('click', () => {
+    valueCalc = [subtract(valueCalc[0], valueCalc[1])]
+})
+
+multiplyButton.addEventListener('click', () => {
+    valueCalc = [multiply(valueCalc[0], valueCalc[1])]
+})
+
+divideButton.addEventListener('click', () => {
+    valueCalc = [divide(valueCalc[0], valueCalc[1])]
+})
+
+equalButton.addEventListener('click', () => {
     operate();
 })
 
@@ -36,23 +49,23 @@ equalButton.addEventListener('click', ()=>{
 
 //Math functions
 
-function add(x, y){
+function add(x, y) {
     return +x + +y
 }
 
-function subtract(x, y){
+function subtract(x, y) {
     return x - y;
 }
 
-function multiply(x,y){
+function multiply(x, y) {
     return x * y;
 }
 
-function divide(x,y){
+function divide(x, y) {
     return x / y;
 }
 
-function operate(){
-    valueShown = parseInt(valueCalc)
+function operate() {
+    valueShown.textContent = valueCalc;
     console.log(parseInt(valueCalc))
 }
